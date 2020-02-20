0ee5f90d3f37489b3d90f7508274bbfeb172a41e
Dan Cornell <dan@denimgroup.com>
2020-02-20 17:58:24 -0600

    Added severity and tags  parsing from the command line

--- automated_exception_reporting/generate_exception_report.py
+++ automated_exception_reporting/generate_exception_report.py
@@ -4,6 +4,37 @@ import csv
 import sys
 from ThreadFixPythonApi import threadfixpro
 
+def make_tags_list(tf_con, tags):
+	ret_val = []
+
+	my_tags = tags.split(',')
+	for tag_name in my_tags:
+		tag_id = tf_con.TagsAPI.get_tag_by_name(tag_name)
+		if tag_id.success:
+			ret_val.append(tag_id.data[0]['id'])
+
+	return(ret_val)
+
+def make_severities_list(severities):
+	ret_val = []
+
+	severity_list = severities.split(',')
+	for severity in severity_list:
+		if(severity == 'Critical'):
+			ret_val.append(5)
+		elif(severity == 'High'):
+			ret_val.append(4)
+		elif(severity == 'Medium'):
+			ret_val.append(3)
+		elif(severity == 'Low'):
+			ret_val.append(2)
+		elif(severity == 'Info'):
+			ret_val.append(1)
+		else:
+			print('Got unknown severity: '  +  severity)
+
+	return(ret_val)
+
 def create_vuln_location(vulnerability):
 	ret_val = 'Unknown'
 
@@ -33,17 +64,23 @@ def create_vuln_record(vulnerability):
 
 	return ret_val
 
-if len(sys.argv) < 4:
-  print('Usage: generate_exception_report.py <tf_server> <api_key> <outfile>')
+if len(sys.argv) < 7:
+  print('Usage: generate_exception_report.py <tf_server> <api_key> <outfile> <tags> <severities> <aging>')
   exit(2)
 
 tf_server = sys.argv[1]
 api_key = sys.argv[2]
 outfile = sys.argv[3]
+tags = sys.argv[4]
+severities = sys.argv[5]
+aging = sys.argv[6]
 
 print('Using tf_server: ' + tf_server)
 print('Using api_key: ' + api_key)
 print('Using outfile: ' + outfile)
+print('Using tags: ' + tags)
+print('Using severities: ' + severities)
+print('Using aging: ' + aging)
 
 # Get our connection to the ThreadFix server
 tfp = threadfixpro.ThreadFixProAPI(tf_server, api_key, verify_ssl=False)
@@ -54,19 +91,15 @@ header_list = ['Vulnerability ID', 'Application', 'Vulnerability Type', 'Severit
 csvout.writerow(header_list)
 
 
-# Optionally - Parse from command-line args
-# In this ThreadFix installation, PCI has tag ID 1, exposure:public has tag ID 6, and env:production has tag ID 7
-# The tags API will allow us to look up tag IDs by name
-tags = [1, 6, 7]
-# Generic severity 5 maps to Critical and 4 maps to High
-generic_severities = [5, 4]
-days_old = 30
+# Look up the tags from the command line
+tags_list = make_tags_list(tfp, tags)
+severities_list = make_severities_list(severities)
 
 page = 0
 num_vulns_in_batch = -1
 
 while num_vulns_in_batch != 0:
-	vulnerabilities = tfp.VulnerabilitiesAPI.vulnerability_search(generic_severities=generic_severities, tags=tags, days_old=days_old, number_vulnerabilities=100, page=page, show_open=True, show_not_false_positive=True)
+	vulnerabilities = tfp.VulnerabilitiesAPI.vulnerability_search(generic_severities=severities_list, tags=tags_list, days_old=aging, number_vulnerabilities=100, page=page, show_open=True, show_not_false_positive=True)
 	if vulnerabilities.success:
 		num_vulns_in_batch = len(vulnerabilities.data)
 		print ('Found ' + str(num_vulns_in_batch) + ' vulnerabilities')
